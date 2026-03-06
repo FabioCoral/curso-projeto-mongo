91ea92f321ca765adf5fcea6f2f2081e432d905c
Fabio Coral <fdcoral@hotmail.com>
2026-03-06 09:45:05 -0300

    Obtendo post por id

---
+++ src/main/java/com/fabiocoral/workshopmongo/services/PostService.java
@@ -0,0 +1,20 @@
+package com.fabiocoral.workshopmongo.services;
+
+import com.fabiocoral.workshopmongo.domain.Post;
+import com.fabiocoral.workshopmongo.repository.PostRepository;
+import com.fabiocoral.workshopmongo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id){
+        Optional<Post> post = repo.findById(id);
+        return post.orElseThrow(() -> new ObjectNotFoundException("Post not found"));
+    }
+}
