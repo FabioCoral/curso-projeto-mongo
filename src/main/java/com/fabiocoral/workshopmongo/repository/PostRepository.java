bc7fa6a1beaeeab32ce0a828f68f5cb67cb36aff
Fabio Coral <fdcoral@hotmail.com>
2026-03-06 08:59:25 -0300

    Criando entity post com User aninhado

---
+++ src/main/java/com/fabiocoral/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,10 @@
+package com.fabiocoral.workshopmongo.repository;
+
+import com.fabiocoral.workshopmongo.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
