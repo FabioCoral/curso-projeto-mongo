dcde0bee24efc9f3a8c06ccc6db3d8f6399f6aa6
Fabio Coral <fdcoral@hotmail.com>
2026-03-06 09:33:05 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/fabiocoral/workshopmongo/resources/UserResource.java
+++ src/main/java/com/fabiocoral/workshopmongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.fabiocoral.workshopmongo.resources;
 
+import com.fabiocoral.workshopmongo.domain.Post;
 import com.fabiocoral.workshopmongo.domain.User;
 import com.fabiocoral.workshopmongo.dto.UserDTO;
 import com.fabiocoral.workshopmongo.services.UserService;
@@ -52,6 +53,12 @@ public class UserResource {
         user.setId(id);
         user = service.update(user);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping(value = "/{id}/posts")
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+        User user = service.findById(id);
+        return ResponseEntity.ok().body(user.getPosts());
     }
 
 
